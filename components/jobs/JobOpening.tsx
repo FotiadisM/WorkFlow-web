fcb63d95c4e610e0c3de17ff005303faeee38b45
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2021-08-16 22:53:58 +0300

    toggle interested on jobs, ui change

--- components/jobs/JobOpening.tsx
+++ components/jobs/JobOpening.tsx
@@ -1,30 +1,127 @@
 import React, { useState } from "react";
 import { classNames } from "../../src/util";
 
+interface Job {
+  id: string;
+  user_id: string;
+  title: string;
+  type: "full_time" | "part_time" | "internship";
+  location: string;
+  company: {
+    company_id: string;
+    company_name: string;
+  };
+  salary: {
+    min: number;
+    max: number;
+  };
+  description: string;
+  skills: string[];
+}
+
 const JobDetail: React.FC<{ name: string }> = ({ name, children }) => {
   return (
-    <div>
+    <>
       <h3 className="text-lg">{name}</h3>
       <hr className="mb-2" />
       {children}
-    </div>
+    </>
   );
 };
 
 interface JobOpeningPros {
-  isOwner: boolean;
+  job: Job;
+  onButtonPress: (
+    job_id: string,
+    buttonType: "interested" | "edit" | "default"
+  ) => void;
+  buttonType: "interested" | "edit" | "default";
 }
 
-export default function JobOpening({ isOwner }: JobOpeningPros) {
-  const [open, setOpen] = useState<boolean>(false);
-  const [edit, setEdit] = useState<boolean>(false);
+export default function JobOpening({
+  job,
+  onButtonPress,
+  buttonType,
+}: JobOpeningPros) {
+  const [openDetails, setOpenDetails] = useState<boolean>(false);
+
+  const jobTypeToText = (t: string): string => {
+    switch (t) {
+      case "full_time":
+        return "Full time";
+      case "part_time":
+        return "Part time";
+      default:
+        return "Internship";
+    }
+  };
+
+  const jobSalaryToText = (min: number, max: number): string => {
+    if (min == max) {
+      return min.toString() + "K";
+    }
+
+    return min.toString() + "K-" + max.toString() + "K";
+  };
+
+  const evaluateButton = () => {
+    switch (buttonType) {
+      case "interested":
+        return (
+          <button
+            className="btn px-3 py-2 text-white bg-purple-800 hover:bg-purple-700 active:bg-purple-900 flex items-center"
+            onClick={() => onButtonPress(job.id, buttonType)}
+          >
+            <span className="mr-2">Interested</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className=""
+              viewBox="0 0 16 16"
+            >
+              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+            </svg>
+          </button>
+        );
+      case "edit":
+        return (
+          <button
+            className="text-gray-800 hover:bg-gray-100 flex items-center btn px-3 py-2 border border-gray-300"
+            onClick={() => onButtonPress(job.id, buttonType)}
+          >
+            <svg
+              className="mr-2 h-5 w-5 text-gray-500"
+              x-description="Heroicon name: solid/pencil"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+            </svg>
+            Edit
+          </button>
+        );
+      default:
+        return (
+          <button
+            className="btn px-3 text-purple-800 py-2 border-2 rounded-md border-purple-800 hover:bg-purple-800 hover:text-white active:bg-purple-900"
+            onClick={() => onButtonPress(job.id, buttonType)}
+          >
+            Interested
+          </button>
+        );
+    }
+  };
 
   return (
     <div style={{ minWidth: "900px" }}>
       <div className="flex justify-between items-center space-x-44 p-2">
         <div>
           <h2 className="text-2xl font-bold leading-7 text-gray-900 mb-2">
-            Back End Developer
+            {job.title}
           </h2>
           <div className="flex items-center space-x-3 text-gray-600">
             <div className="flex items-center">
@@ -39,7 +136,7 @@ export default function JobOpening({ isOwner }: JobOpeningPros) {
                 <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5z" />
                 <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z" />
               </svg>
-              Full time
+              {jobTypeToText(job.type)}
             </div>
             <div className="flex items-center">
               <svg
@@ -52,7 +149,7 @@ export default function JobOpening({ isOwner }: JobOpeningPros) {
               >
                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
               </svg>
-              Remote
+              {job.location}
             </div>
             <div className="flex items-center">
               <svg
@@ -70,7 +167,7 @@ export default function JobOpening({ isOwner }: JobOpeningPros) {
                   clipRule="evenodd"
                 ></path>
               </svg>
-              120K-140K
+              {jobSalaryToText(job.salary.min, job.salary.max)}
             </div>
             <div className="flex items-center">
               <svg
@@ -87,16 +184,16 @@ export default function JobOpening({ isOwner }: JobOpeningPros) {
                 />
                 <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z" />
               </svg>
-              Google
+              {job.company.company_name}
             </div>
           </div>
         </div>
         <div className="flex items-center space-x-4">
           <button
             className="flex items-center btn px-3 py-2 border border-gray-300 text-gray-800 hover:bg-gray-100"
-            onClick={() => setOpen((o) => !o)}
+            onClick={() => setOpenDetails((o) => !o)}
           >
-            {open ? (
+            {openDetails ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -121,69 +218,28 @@ export default function JobOpening({ isOwner }: JobOpeningPros) {
             )}
             View
           </button>
-          {isOwner ? (
-            <button
-              className={classNames(
-                edit
-                  ? "bg-purple-800 text-white hover:bg-purple-600"
-                  : "text-gray-800 hover:bg-gray-100",
-                "flex items-center btn px-3 py-2 border border-gray-300"
-              )}
-              onClick={() => {
-                setEdit((e) => !e);
-                if (!open) {
-                  setOpen(true);
-                }
-              }}
-            >
-              <svg
-                className={classNames(
-                  edit ? "" : "text-gray-500",
-                  "mr-2 h-5 w-5"
-                )}
-                x-description="Heroicon name: solid/pencil"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-              </svg>
-              Edit
-            </button>
-          ) : (
-            <button className="btn px-3 py-2 text-white bg-purple-800 hover:bg-purple-700 active:bg-purple-900">
-              I'm interested
-            </button>
-          )}
+          {evaluateButton()}
         </div>
       </div>
-      {/* {open ? ( */}
       <div
         className={
-          (classNames("pt-3 pb-4 pl-2 space-y-4"), open ? "" : "hidden")
+          (classNames("pt-3 pb-4 space-y-10"),
+          openDetails ? "px-4 space-y-1" : "hidden")
         }
       >
         <JobDetail name={"Description"}>
-          <p className="text-gray-700 max-w-3xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <p className="text-gray-700 max-w-3xl">{job.description}</p>
         </JobDetail>
         <JobDetail name={"Prefered Skills"}>
-          <div className="flex space-x-2">
-            {["C", "C++", "Go", "Python"].map((i) => (
-              <div className="py-1 px-2 bg-gray-200 rounded-md">{i}</div>
+          <div className="flex space-x-2 py-2">
+            {job.skills.map((s, i) => (
+              <div key={i} className="py-1 px-2 bg-gray-200 rounded-md">
+                {s}
+              </div>
             ))}
           </div>
         </JobDetail>
       </div>
-      {/* ) : null} */}
     </div>
   );
 }
--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,7 +1,136 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JobOpening from "@/components/jobs/JobOpening";
 import Navbar from "@/components/navbar/Navbar";
 import { classNames } from "@/src/util";
+
+interface Job {
+  id: string;
+  user_id: string;
+  title: string;
+  type: "full_time" | "part_time" | "internship";
+  location: string;
+  company: {
+    company_id: string;
+    company_name: string;
+  };
+  salary: {
+    min: number;
+    max: number;
+  };
+  description: string;
+  skills: string[];
+}
+
+const dummyJobs: Job[] = [
+  {
+    id: "0",
+    user_id: "1",
+    title: "Job Title",
+    type: "full_time",
+    location: "Remote",
+    company: {
+      company_id: "0",
+      company_name: "Google",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "Hello this is a very wonderfull description",
+    skills: ["C++", "Go", "Python", "Docker"],
+  },
+  {
+    id: "1",
+    user_id: "1",
+    title: "Job Title 2",
+    type: "full_time",
+    location: "Athens, Greece",
+    company: {
+      company_id: "0",
+      company_name: "",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "",
+    skills: [],
+  },
+  {
+    id: "2",
+    user_id: "1",
+    title: "Job Title 3",
+    type: "full_time",
+    location: "",
+    company: {
+      company_id: "0",
+      company_name: "",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "",
+    skills: [],
+  },
+];
+
+const dummyInterestedJobs: Job[] = [
+  {
+    id: "0",
+    user_id: "1",
+    title: "Interested Job Title",
+    type: "full_time",
+    location: "Remote",
+    company: {
+      company_id: "0",
+      company_name: "Google",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "Hello this is a very wonderfull description",
+    skills: ["C++", "Go", "Python", "Docker"],
+  },
+  {
+    id: "1",
+    user_id: "1",
+    title: "Interested Job Title 2",
+    type: "full_time",
+    location: "Athens, Greece",
+    company: {
+      company_id: "0",
+      company_name: "",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "",
+    skills: [],
+  },
+];
+
+const dummyUserJobs: Job[] = [
+  {
+    id: "2",
+    user_id: "1",
+    title: "User Jobs Title",
+    type: "full_time",
+    location: "",
+    company: {
+      company_id: "0",
+      company_name: "",
+    },
+    salary: {
+      min: 40,
+      max: 80,
+    },
+    description: "",
+    skills: [],
+  },
+];
 
 const sideBar: { name: string }[] = [
   { name: "Career Opportunities " },
@@ -11,21 +140,74 @@ const sideBar: { name: string }[] = [
 
 export default function Jobs() {
   const [curPage, setCurPage] = useState<string>(sideBar[0].name);
-  // const [navHeight, setNavHeight] = useState<number>(0);
+  const [currJobs, setCurrJobs] = useState<Job[]>([]);
+  const [interestedJobs, setInterestedJobs] = useState<Job[]>([]);
+  const [searchJobs, setSearchJobs] = useState<Job[]>([]);
+  const [userJobs, setUserJobs] = useState<Job[]>([]);
 
-  // const getHeight = (h: number) => {
-  //   setNavHeight(h);
-  // };
+  const onButtonPress = (
+    job_id: string,
+    buttonType: "interested" | "edit" | "default"
+  ) => {
+    switch (buttonType) {
+      case "interested":
+        setInterestedJobs((old) => old.filter((j) => job_id !== j.id));
+        // let tmpJobs: Job[] = [];
+        // for (let j of interestedJobs) {
+        //   if (job_id !== j.id) {
+        //     tmpJobs.push(j);
+        //   }
+        //   setInterestedJobs(tmpJobs);
+        // }
+        break;
+      case "edit":
+        break;
+      default:
+        for (let j of searchJobs) {
+          if (job_id === j.id) {
+            setInterestedJobs((o) => [j, ...o]);
+          }
+        }
+    }
+  };
 
-  // const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    if (curPage === sideBar[0].name || curPage === sideBar[1].name) {
+      // fetch interested jobs
+      setInterestedJobs(dummyInterestedJobs);
+    }
+
+    if (curPage === sideBar[0].name) {
+      // fetch search jobs
+      setSearchJobs(dummyJobs);
+      setCurrJobs(dummyJobs);
+    } else if (curPage === sideBar[1].name) {
+      setCurrJobs(dummyInterestedJobs);
+    } else {
+      // fetch userJobs
+      setUserJobs(dummyUserJobs);
+      setCurrJobs(dummyUserJobs);
+    }
+  }, [curPage]);
+
+  const evaluteButtonType = (
+    job_id: string
+  ): "interested" | "edit" | "default" => {
+    if (curPage == sideBar[2].name) {
+      return "edit";
+    }
+
+    for (let j of interestedJobs) {
+      if (job_id === j.id) return "interested";
+    }
+
+    return "default";
+  };
 
   return (
     <>
       <Navbar />
-      <main
-        className="flex justify-center mt-10"
-        style={{ minHeight: "480px" }}
-      >
+      <main className="flex justify-center mt-10">
         <div className="px-7 border-r space-y-1">
           {sideBar.map((i) => (
             <h1
@@ -46,9 +228,19 @@ export default function Jobs() {
           <h1 className="text-4xl font-semibold">{curPage}</h1>
           <hr className="mt-2 mb-4 border shadow-md" />
           <div className="space-y-1">
-            <JobOpening isOwner={curPage === "My Job Postings"} />
-            <hr />
-            <JobOpening isOwner={curPage === "My Job Postings"} />
+            {currJobs.map((j, i) => {
+              return (
+                <>
+                  {i !== 0 ? <hr /> : null}
+                  <JobOpening
+                    key={j.id}
+                    job={j}
+                    onButtonPress={onButtonPress}
+                    buttonType={evaluteButtonType(j.id)}
+                  />
+                </>
+              );
+            })}
           </div>
         </div>
       </main>
